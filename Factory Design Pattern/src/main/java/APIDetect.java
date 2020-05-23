6f94973797a5b9a1da0283ad0e2fbf3d45f24a30
Shubham Kumar <31592194+shubhamk0027@users.noreply.github.com>
2020-05-23 16:27:33 +0530

    Update APIDetect.java

--- Factory Design Pattern/src/main/java/APIDetect.java
+++ Factory Design Pattern/src/main/java/APIDetect.java
@@ -4,9 +4,7 @@ public abstract class APIDetect implements APIInterface {
     public abstract String detect(String Text) throws Exception;
 
     public Boolean Validate(String text) {
-        // Exception will not be thrown here!
         if (text==null) { throw new NullPointerException("Text Can Not Be Null");}
-        /* Logic To Validate The Text Common To All The APIS*/
         return Boolean.TRUE;
     }
 
