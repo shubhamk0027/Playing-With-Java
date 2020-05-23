e62c2b591134d044bb10eda227a155de7b12d432
Shubham Kumar <31592194+shubhamk0027@users.noreply.github.com>
2020-05-23 16:25:02 +0530

    Update Worker.java

--- Concurrency/src/main/java/Worker.java
+++ Concurrency/src/main/java/Worker.java
@@ -1,14 +1,11 @@
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
-
-// see the test
 /*
 The Runnable interface should be implemented by any class
 whose instances are intended to be executed by a thread.
 The class must define a method of no arguments called run.
 https://www.baeldung.com/java-countdown-latch
 */
-// NOT a thread, but a thread runnable class!
 
 public class Worker implements Runnable{
         private List<String> outScraper;
